036d9f0c4121e91f2fe017443f97b12c460f50b3
Nargiza <91196801+nematova72@users.noreply.github.com>
2025-03-10 17:42:02 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from game import Game
+
+if __name__ == "__main__":
+    player_names = ["Ali", "Bob", "Sardor"]  # O'yinchilar ro'yxati
+    game = Game(player_names)  # O'yinni yaratish
+    game.play()  # O'yinni boshlash
